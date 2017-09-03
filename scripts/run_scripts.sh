bfbb4087fe585fa9948559bc2c3e3ca123d378d9
Patrick Liesveld <pliesveld@users.noreply.github.com>
2017-09-03 17:03:51 -0400

    watcher sets cwd to the script dir

--- scripts/run_scripts.sh
+++ scripts/run_scripts.sh
@@ -4,7 +4,9 @@ inotifywait -r -m -e modify /scripts |
 while read path _ file; do 
 	[[ $file =~ ^.*sh$ ]] && {
 		echo '***************************' 
-		bash $file;
+		bash $path$file
 	}
 done
+
+
 
